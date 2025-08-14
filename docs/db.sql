9b0b4c32919a27785879a9f2c4a2355de107fa91
reyesuwu461 <samuelreyescastro456@gmail.com>
2025-08-14 12:25:43 -0500

    db.sql

--- docs/db.sql
+++ docs/db.sql
@@ -1,3 +1,5 @@
+-- Full Database Creation: Payment System (English version with comments)
+
 -- Create database
 CREATE DATABASE payment_system;
 USE payment_system;
@@ -12,3 +14,5 @@ CREATE TABLE clients (
     email VARCHAR(100) NOT NULL,
     CONSTRAINT chk_email_format CHECK (email LIKE '%_@__%.__%')
 );
+
+-- registered_at
